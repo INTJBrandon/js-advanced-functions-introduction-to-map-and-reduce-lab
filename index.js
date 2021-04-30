c818344c171cbad11fe3058c3c444da2843ad54e
seireitou <ausis@mail.com>
2021-04-29 22:24:15 -0500

    commit tree e84a962c856ae6113705ff3b391f9e87653d3f8a

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(arr) {
+    const result = []
+    arr.forEach(element => {
+        result.push(-element)
+    })
+    return result
+}
+
+function mapToNoChange(arr) {
+    const result = []
+    arr.forEach(element => {
+        result.push(element)
+    })
+    return result
+}
+
+function mapToDouble(arr) {
+    const result = []
+    arr.forEach(element => {
+        result.push(element * 2)
+    })
+    return result
+}
+
+function mapToSquare(arr) {
+    const result = []
+    arr.forEach(element => {
+        result.push(element * element)
+    })
+    return result
+}
+
+function reduceToTotal(arr, startingPoint = 0) {
+    arr.forEach(element => {
+        startingPoint += element
+    })
+    return startingPoint
+}
+
+function reduceToAllTrue(arr) {
+    for(const element of arr) {
+        if(!element) {
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    for(const element of arr) {
+        if(!!element) return true
+    }
+    return false
+}
